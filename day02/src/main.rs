ccc2672a24790b5ce1a361bfd549b0e4dfcb2148
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-12-03 22:36:24 +0100

    Add 2nd solution of the day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,34 +1,40 @@
-#[derive(Debug, Clone)]
-struct Submarine {
-    pub depth: i32,
-    pub horizontal: i32,
-}
-
-impl Submarine {
-    pub fn new(depth: i32, horizontal: i32) -> Self {
-        Self { depth, horizontal }
-    }
-
-    /// Moves the submarine with the given steps
-    pub fn do_move(&self, moves: &[Move]) -> (i32, i32) {
-        let Self { mut depth, mut horizontal } = &self;
-        for step in moves {
-            match step {
-                Move::Forward(v) => horizontal += v,
-                Move::Down(v) => depth += v,
-                Move::Up(v) => depth -= v,
-            }
-        }
-        (depth, horizontal)
-    }
-}
-
 /// States the submarine can move
 #[derive(Debug)]
-enum Move {
-    Forward(i32),
-    Down(i32),
-    Up(i32),
+pub enum Move {
+    Forward(i64),
+    Down(i64),
+    Up(i64),
+}
+
+/// Moves the submarine with the given steps
+pub fn do_move(moves: &[Move]) -> (i64, i64) {
+    let (mut depth, mut horizontal) = (0, 0);
+    for step in moves {
+        match step {
+            Move::Forward(x) => horizontal += x,
+            Move::Down(x) => depth += x,
+            Move::Up(x) => depth -= x,
+        }
+    }
+    (depth, horizontal)
+}
+
+/// Aims the submarine rather than move with the given steps
+pub fn do_aim(moves: &[Move]) -> (i64, i64) {
+    let (mut depth, mut horizontal, mut aim) = (0, 0, 0);
+
+    for step in moves {
+        match step {
+            Move::Forward(x) => {
+                horizontal += x;
+                depth += aim * x;
+            }
+            Move::Down(x) => aim += x,
+            Move::Up(x) => aim -= x,
+        }
+    }
+
+    (depth, horizontal)
 }
 
 /// Parse input
@@ -39,7 +45,7 @@ fn parse(instructions: &str) -> Vec<Move> {
         .filter(|&line| !line.is_empty())
         .map(|line| {
             if let Some((movement, value)) = line.split_once(" ") {
-                let step = value.parse::<i32>().expect("Failed to parse integer");
+                let step = value.parse::<i64>().expect("Failed to parse integer");
                 match movement {
                     "forward" => Move::Forward(step),
                     "down" => Move::Down(step),
@@ -55,30 +61,39 @@ fn parse(instructions: &str) -> Vec<Move> {
 
 fn main() {
     let instructions = parse(include_str!("input.txt"));
-    let submarine = Submarine::new(0, 0);
-    let (depth, horizontal) = submarine.do_move(&instructions);
+    let (depth, horizontal) = do_move(&instructions);
+    dbg!(depth * horizontal);
 
+    let (depth, horizontal) = do_aim(&instructions);
     dbg!(depth * horizontal);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse, Submarine};
+    use crate::{do_aim, do_move, parse};
+
+    const INSTRUCTIONS: &str = r#"
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+    "#;
 
     #[test]
-    fn follows_movement() {
-        let instructions = r#"
-            forward 5
-            down 5
-            forward 8
-            up 3
-            down 8
-            forward 2
-        "#;
-        let instructions = parse(&instructions);
-        let submarine = Submarine::new(0, 0);
-        let (depth, horizontal) = submarine.do_move(&instructions);
+    fn test_do_move() {
+        let instructions = parse(&INSTRUCTIONS);
+        let (depth, horizontal) = do_move(&instructions);
         assert_eq!(15, horizontal);
         assert_eq!(10, depth);
+    }
+
+    #[test]
+    fn test_do_aim() {
+        let instructions = parse(&INSTRUCTIONS);
+        let (depth, horizontal) = do_aim(&instructions);
+        assert_eq!(15, horizontal);
+        assert_eq!(60, depth);
     }
 }
