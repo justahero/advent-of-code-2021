0104cfd1061b2f3b9642a96ac44c575e3703eb40
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2021-12-03 20:15:50 +0100

    Solution for 2nd half of first day

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -17,19 +17,39 @@ pub fn count_single(depths: &[i32]) -> usize {
         .count()
 }
 
+pub fn count_in_threes(depths: &[i32]) -> usize {
+    depths
+        .iter()
+        .tuple_windows()
+        .into_iter()
+        .map(|(a, b, c)| a + b + c)
+        // .inspect(|x| println!("{}", x))
+        .tuple_windows()
+        .into_iter()
+        .filter(|(x, y)| x < y)
+        .count()
+}
+
 fn main() {
     let depths = parse(include_str!("input.txt"));
-    let count = count_single(&depths);
-    dbg!(count);
+
+    dbg!(count_single(&depths));
+    dbg!(count_in_threes(&depths));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::count_single;
+    use crate::{count_single, count_in_threes};
 
     #[test]
     fn test_count_increases() {
         let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
         assert_eq!(7, count_single(&depths));
+    }
+
+    #[test]
+    fn test_count_in_threes_increases() {
+        let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(5, count_in_threes(&depths));
     }
 }
