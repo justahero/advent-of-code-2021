19be9a592ec4147b21a2faf4a5a2ef85415e92ac
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-12-04 12:36:04 +0100

    Try to implement logic to find ratings for 2nd half

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,31 +1,82 @@
-use std::ops::Shl;
+use std::{ops::Shl, fmt::{Formatter, Result, Debug}};
 
 use anyhow::anyhow;
 
+#[derive(Clone, Copy)]
+struct Binary(u32);
+
+impl Debug for Binary {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:05b}", self.0)
+    }
+}
+
+impl Binary {
+    pub fn new(value: u32) -> Self {
+        Self(value)
+    }
+}
+
 #[derive(Debug)]
 struct BinaryList {
-    pub binaries: Vec<u32>,
+    pub binaries: Vec<Binary>,
     pub count: usize,
 }
 
+fn bit_is_unset(pos: usize, binary: u32) -> bool {
+    binary & 1_u32.shl(pos - 1) == 0
+}
+
+fn bit_is_set(pos: usize, binary: u32) -> bool {
+    binary & 1_u32.shl(pos - 1) > 0
+}
+
 impl BinaryList {
-    pub fn new(binaries: Vec<u32>, count: usize) -> Self {
+    pub fn new(binaries: Vec<Binary>, count: usize) -> Self {
         Self { binaries, count }
     }
 
     /// Find oxygen generator & CO2 scrubber ratings
     /// The 2nd part of the day
-    pub fn find_oxygen_co2scrubber_ratings(&self) {
+    pub fn find_oxygen_co2scrubber_ratings(&self) -> (u32, u32) {
+        let oxygen = Self::find_rating(self.count, &self.binaries, bit_is_unset);
+        let co2 = Self::find_rating(self.count - 1, &self.binaries, bit_is_set);
+        (oxygen, co2)
+    }
 
+    /// Partitions the
+    fn find_rating<'a, F>(position: usize, list: &[Binary], f: F) -> u32
+    where
+        F: Fn(usize, u32) -> bool,
+    {
+        println!("FIND RATING: position: {}, list: {:?}", position, list);
+        let (left, right): (Vec<Binary>, Vec<Binary>) = list.iter().partition(|&&bin| f(position, bin.0));
+        println!("  LEFT: {:?}", left);
+        println!("  RIGHT: {:?}", right);
+
+        if left.len() <= right.len() {
+            if right.len() == 1 {
+                return right[0].0;
+            }
+            Self::find_rating(position - 1, &right, f)
+        } else {
+            if left.len() == 1 {
+                return left[0].0;
+            }
+            Self::find_rating(position - 1, &left, f)
+        }
     }
 
     /// Determines gamma & epsilon ratings
     pub fn find_gama_epsilon_ratings(&self) -> (u32, u32) {
-        let gamma = self.count()
+        let gamma = self
+            .count()
             .iter()
             .enumerate()
             .filter(|(_pos, (zeros, ones))| ones > zeros)
-            .fold(0_u32, |gamma, (pos, _)| gamma + 1_u32.shl(self.count - pos - 1));
+            .fold(0_u32, |gamma, (pos, _)| {
+                gamma + 1_u32.shl(self.count - pos - 1)
+            });
 
         let epsilon = !gamma & (1_u32.shl(self.count) - 1);
 
@@ -44,11 +95,12 @@ impl BinaryList {
         result
     }
 
-    /// Group binaries by ones & zeros
-    fn group_binaries(&self, position: usize) -> (Vec<u32>, Vec<u32>) {
+    /// Group binaries by ones & zeros at given position.
+    /// Returns tuple of (zeros, ones)
+    fn group_binaries(&self, position: usize) -> (Vec<Binary>, Vec<Binary>) {
         self.binaries
             .iter()
-            .partition(|&bin| bin & 1_u32.shl(position) == 0)
+            .partition(|&bin| bin.0 & 1_u32.shl(position) == 0)
     }
 }
 
@@ -62,19 +114,26 @@ fn parse_input(input: &str) -> anyhow::Result<BinaryList> {
 
     let binaries = lines
         .iter()
-        .map(|line| u32::from_str_radix(line, 2))
-        .filter_map(Result::ok)
+        .filter_map(|line| u32::from_str_radix(line, 2).ok())
+        .map(Binary::new)
         .collect::<Vec<_>>();
 
-    let count = lines.first().ok_or(anyhow!("Failed to get first element."))?.len();
+    let count = lines
+        .first()
+        .ok_or(anyhow!("Failed to get first element."))?
+        .len();
 
     Ok(BinaryList::new(binaries, count))
 }
 
 fn main() -> anyhow::Result<()> {
     let input = parse_input(include_str!("input.txt"))?;
+
     let (gamma, epsilon) = input.find_gama_epsilon_ratings();
     dbg!(gamma * epsilon);
+
+    let (oxygen, co2) = input.find_oxygen_co2scrubber_ratings();
+    dbg!(oxygen * co2);
 
     Ok(())
 }
@@ -103,16 +162,7 @@ mod tests {
         let binary_list = parse_input(INPUT).expect("Failed to parse input.");
         let list = binary_list.count();
         assert_eq!(binary_list.count, list.len());
-        assert_eq!(
-            vec![
-                (5, 7),
-                (7, 5),
-                (4, 8),
-                (5, 7),
-                (7, 5),
-            ],
-            list,
-        );
+        assert_eq!(vec![(5, 7), (7, 5), (4, 8), (5, 7), (7, 5),], list,);
     }
 
     #[test]
@@ -125,6 +175,9 @@ mod tests {
 
     #[test]
     fn find_oxygen_rating() {
-
+        let binary_list = parse_input(INPUT).expect("Failed to parse input.");
+        let (oxygen, co2) = binary_list.find_oxygen_co2scrubber_ratings();
+        assert_eq!(23, oxygen);
+        assert_eq!(10, co2);
     }
 }
