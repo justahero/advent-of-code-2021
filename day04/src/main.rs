23615023393fe5177a7266dd67a3ce4973120f92
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2021-12-04 15:18:00 +0100

    Parse input to numbers & boards

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,17 +2,44 @@ use anyhow::anyhow;
 
 #[derive(Debug)]
 struct Board {
-    
+    pub fields: Vec<u32>,
+}
+
+impl Board {
+    pub fn new(fields: Vec<u32>) -> Self {
+        Self { fields }
+    }
+
+    pub fn is_marked(&self, numbers: &[u32]) -> Option<Vec<u32>> {
+        None
+    }
+}
+
+impl TryFrom<&str> for Board {
+    type Error = anyhow::Error;
+
+    fn try_from(line: &str) -> Result<Self, Self::Error> {
+        let numbers = line
+            .split_ascii_whitespace()
+            .map(str::trim)
+            .map(|val| {
+                val.parse::<u32>()
+                    .map_err(|_| anyhow!("Failed to parse value."))
+            })
+            .collect::<Result<Vec<u32>, Self::Error>>()?;
+        Ok(Board::new(numbers))
+    }
 }
 
 #[derive(Debug)]
 struct BingoSubsystem {
-
+    pub numbers: Vec<u32>,
+    pub boards: Vec<Board>,
 }
 
 impl BingoSubsystem {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(numbers: Vec<u32>, boards: Vec<Board>) -> Self {
+        Self { numbers, boards }
     }
 }
 
@@ -22,14 +49,29 @@ impl BingoSubsystem {
 /// * an empty line separates the Bingo boards from each other
 /// * each board contains of 5x5 numbers
 fn parse_input(input: &str) -> anyhow::Result<BingoSubsystem> {
-    let blocks = input
-        .split("\n\n")
-        .map(str::trim)
-        .collect::<Vec<_>>();
+    let blocks = input.split("\n\n").map(str::trim).collect::<Vec<_>>();
 
     println!("BLOCKS: {:?}", blocks);
+    let numbers = blocks
+        .first()
+        .ok_or_else(|| anyhow!("No bingo numbers found."))?
+        .split(",")
+        .map(str::trim)
+        .map(|value| value.parse::<u32>())
+        .filter_map(Result::ok)
+        .collect::<Vec<_>>();
 
-    Ok(BingoSubsystem::new())
+    println!("NUMBERS: {:?}", numbers);
+
+    let boards = blocks
+        .iter()
+        .skip(1)
+        .map(|&line| Board::try_from(line))
+        .collect::<Result<Vec<_>, anyhow::Error>>()?;
+
+    println!("BOARDS: {:?}", boards);
+
+    Ok(BingoSubsystem::new(numbers, boards))
 }
 
 fn main() -> anyhow::Result<()> {
@@ -67,5 +109,13 @@ mod tests {
     #[test]
     fn parses_input_successfully() {
         let bingo = parse_input(INPUT).expect("Failed to parse input.");
+        assert_eq!(
+            vec![
+                7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8,
+                19, 3, 26, 1
+            ],
+            bingo.numbers,
+        );
+        assert_eq!(3, bingo.boards.len(),);
     }
 }
