c70db28b803868d44f8a53fe43e0f832e500b486
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2021-12-14 18:42:04 +0100

    Implement parse function to read content from file

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,21 +1,34 @@
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct Rule(String, char);
 
 #[derive(Debug)]
 struct Polymer {
     pub template: String,
-    pub pairs: Vec<(String, char)>,
+    pub pairs: Vec<Rule>,
 }
 
 impl Polymer {
-    pub fn new(template: &str) -> Self {
-        Self { template: template.to_string(), pairs: Vec::new() }
+    pub fn new(template: &str, pairs: Vec<Rule>) -> Self {
+        Self { template: template.to_string(), pairs }
     }
 }
 
 fn parse_input(input: &str) -> Polymer {
-    let x = input
+    let lines = input
         .lines()
         .map(str::trim)
-        .filter(|&line| !line.is_empty());
+        .filter(|&line| !line.is_empty())
+        .collect_vec();
+
+    let template = lines[0];
+    let pairs = lines[1..].iter().map(|&line| {
+        let (pair, c) = line.split_once(" -> ").expect("Failed to parse");
+        Rule(pair.to_string(), c.chars().next().unwrap())
+    }).collect_vec();
+
+    Polymer::new(template, pairs)
 }
 
 fn main() {
@@ -50,5 +63,11 @@ mod tests {
     #[test]
     fn check_parse_input() {
         let input = parse_input(INPUT);
+        assert_eq!(String::from("NNCB"), input.template);
+        assert_eq!(16, input.pairs.len());
+    }
+
+    #[test]
+    fn test_single_step_process() {
     }
 }
