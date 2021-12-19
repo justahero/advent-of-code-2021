193710b88b60ddce088b746318d61bef30a76768
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-12-19 15:43:36 +0100

    Add debug output to check logic

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -22,21 +22,26 @@ impl<'a> BinaryCursor {
         self.index / 8 >= self.bytes.len()
     }
 
-    pub fn read_bits(&mut self, bits: usize) -> u8 {
-        assert!(bits <= 8);
+    // 11010010_11111110_00101000
+    pub fn read_bits(&mut self, bits: usize) -> u16 {
+        assert!(bits <= 16);
 
         // TODO refactor later, it's a bit cluttered
-        let mut result = 0;
+        let mut result = 0_u16;
         for i in 0..bits {
             let byte_index = (self.index + i) / 8;
             let bit_index = 7 - (self.index + i) % 8;
 
             let is_set = self.bytes[byte_index] & 1_u8.shl(bit_index as u32) > 0;
             if is_set {
-                result = result | 1_u8.shl(bits - 1 - i);
+                result = result | 1_u16.shl(bits - 1 - i);
             }
+
+            // println!("  byte: {}, bit: {} = {}", byte_index, bit_index, is_set);
         }
         self.index += bits;
+
+        println!("Read {} bits: {:0width$b}", bits, result, width = bits);
 
         result
     }
@@ -48,9 +53,9 @@ impl<'a> BinaryCursor {
 
     /// Reads the 5 bit literal, returns a tuple containing a bool to indicate to continue reading the literal
     /// and 4 bits that make up the literal.
-    pub fn read_literal(&mut self) -> (bool, u8) {
+    pub fn read_literal(&mut self) -> (bool, u16) {
         let bits = self.read_bits(5);
-        (bits & 0b10000 > 1, bits & 0xF)
+        (bits & 0b10000 >= 1, bits & 0xF)
     }
 }
 
@@ -74,7 +79,7 @@ struct BinaryReader {
 }
 
 impl BinaryReader {
-    const LITERAL: u8 = 0b100;
+    const LITERAL: u16 = 0b100;
 
     pub fn new(input: &str) -> Self {
         Self {
@@ -83,12 +88,18 @@ impl BinaryReader {
     }
 
     pub fn decode(&self) {
-        let mut cursor = BinaryCursor::new(&self.input.as_bytes()[..]);
+        let mut cursor = BinaryCursor::from(self.input.as_str());
+        Self::parse(&mut cursor);
+    }
 
+    // Parses the binary input
+    fn parse(cursor: &mut BinaryCursor) {
         while !cursor.is_empty() {
             // read packet header
             let version = cursor.read_bits(3);
             let type_id = cursor.read_bits(3);
+
+            println!("VERSION: {}, Type ID: {}", version, type_id);
 
             match type_id {
                 Self::LITERAL => {
@@ -96,14 +107,26 @@ impl BinaryReader {
                     let mut result = 0_u64;
                     loop {
                         let (next, literal) = cursor.read_literal();
+                        println!("  parse literal: {}, {}", next, literal);
                         result = result.shl(4) + literal as u64;
                         if !next {
                             break;
                         }
                     }
+                    cursor.seek_next_byte();
                     println!("Literal: {}", result);
                 }
-                _ => panic!(),
+                _ => {
+                    // read operator and sub packets
+                    let mode = cursor.read_bits(1);
+                    if mode == 0 {
+                        let total_length = cursor.read_bits(15);
+                        println!("Total length: {}", total_length);
+                    } else {
+                        let num_packets = cursor.read_bits(11);
+                        println!("Num packets: {}", num_packets);
+                    }
+                }
             }
         }
     }
@@ -116,7 +139,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::BinaryCursor;
+    use crate::{BinaryCursor, BinaryReader};
 
     #[test]
     fn check_cursor_read_bits() {
@@ -152,5 +175,18 @@ mod tests {
     fn check_parse_cursor_from_string() {
         let cursor = BinaryCursor::from("110100101111111000101000");
         assert_eq!(vec![0b11010010_u8, 0b11111110, 0b00101000], cursor.bytes);
+    }
+
+    #[test]
+    fn decode_input_string() {
+        let reader = BinaryReader::new("110100101111111000101000");
+        reader.decode();
+    }
+
+    #[test]
+    fn decodes_input_with_operator() {
+        let input = "00111000000000000110111101000101001010010001001000000000";
+        let reader = BinaryReader::new(input);
+        reader.decode();
     }
 }
