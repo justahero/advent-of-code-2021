2c6654bb93df9994eee33c644b3d2b196e85ee2a
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-12-22 21:42:57 +0100

    Set up basic loop to get shared beacons

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::HashSet;
+
 use itertools::Itertools;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Point {
     pub x: i32,
     pub y: i32,
@@ -31,36 +33,55 @@ struct Report {
 }
 
 impl Report {
-    pub fn new() -> Self {
-        Self {
-            points: Vec::new(),
-        }
+    /// Finds and possibly returns shared beacons
+    ///
+    /// First bring find the best alignment of the 2nd list of points, if there
+    /// are more than 12 shared beacons, return this list.
+    pub fn shared_beacons(&self, other: &Vec<Point>) -> Option<HashSet<Point>> {
+        // TODO
+        // get all combinations of alignments?
+
+        Some(HashSet::new())
     }
 }
 
 impl From<&str> for Report {
     fn from(line: &str) -> Self {
-        // skip first line
         let points = line.lines().skip(1).map(Point::from).collect_vec();
         Self { points }
     }
 }
 
+/// Retruns the number of shared beacons
+fn shared_beacons(reports: Vec<Report>) -> usize {
+    let beacons = reports
+        .iter()
+        .tuple_combinations()
+        .fold(HashSet::new(), |mut result, (left, right)| {
+            if let Some(shared) = left.shared_beacons(&right.points) {
+                result.extend(shared);
+            }
+            result
+        });
+
+    beacons.len()
+}
+
 /// Parses the list of scanner reports
 fn parse_input(input: &str) -> Vec<Report> {
-    input
-        .split("\n\n")
-        .map(Report::from)
-        .collect_vec()
+    input.split("\n\n").map(Report::from).collect_vec()
 }
 
 fn main() {
-    println!("Hello, world!");
+    let reports = parse_input(include_str!("input.txt"));
+
+    // get first solution
+    dbg!(shared_beacons(reports));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::parse_input;
+    use crate::{parse_input, shared_beacons};
 
     const INPUT: &str = include_str!("example.txt");
 
@@ -68,5 +89,11 @@ mod tests {
     fn parse_scanner_input() {
         let reports = parse_input(INPUT);
         assert_eq!(5, reports.len());
+    }
+
+    #[test]
+    fn test_shared_number_of_beacons() {
+        let reports = parse_input(INPUT);
+        assert_eq!(79, shared_beacons(reports));
     }
 }
