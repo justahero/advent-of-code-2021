d53994045c60b1c85b999d837afcd617cb805153
Sebastian Ziebell <sebastian.ziebell@gmail.com>
2021-12-24 20:40:28 +0100

    Implement game to find winner

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,39 +1,47 @@
+use std::fmt::Display;
+
 use itertools::Itertools;
 
 trait Roll {
-    fn roll(&self) -> Vec<u32>;
+    fn roll(&mut self, time: u32) -> Vec<u32>;
 }
 
 struct DeterministicDice {
     pub start: u32,
+    pub count: u32,
 }
 
 impl Default for DeterministicDice {
     fn default() -> Self {
-        Self { start: 1 }
+        Self { start: 0, count: 0 }
     }
 }
 
 impl Roll for DeterministicDice {
-    fn roll(&self) -> Vec<u32> {
-        let values = [
-            (self.start + 1) % 100,
-            (self.start + 2) % 100,
-            (self.start + 3) % 100,
-        ];
-        values.iter().cloned().collect_vec()
+    fn roll(&mut self, times: u32) -> Vec<u32> {
+        let values = (0..times).map(|step| ((self.start + 1) % 100) + 1).collect_vec();
+        self.start += times;
+        values
     }
 }
 
 #[derive(Debug)]
 struct Player {
-    pub start: u8,
+    pub pos: u32,
     pub score: u32,
 }
 
 impl Player {
     pub fn new(start: u8) -> Self {
-        Self { start, score: 0 }
+        Self { pos: start as u32, score: 0 }
+    }
+
+    /// Apply the rolled die, move position and return current score
+    pub fn play(&mut self, die: &[u32]) -> u32 {
+        let sum = die.iter().sum::<u32>();
+        self.pos = ((self.pos - 1 + sum) % 10) + 1;
+        self.score += self.pos;
+        self.score
     }
 }
 
@@ -41,6 +49,7 @@ impl Player {
 struct Game {
     pub player1: Player,
     pub player2: Player,
+    pub roll_count: u32,
 }
 
 impl Game {
@@ -48,19 +57,38 @@ impl Game {
         Self {
             player1: Player::new(player1),
             player2: Player::new(player2),
+            roll_count: 0,
         }
     }
 
-    pub fn play(&mut self, dice: impl Roll) -> &Player {
-        // play until player arrives
-        &self.player1
+    /// Plays until one player wins, the returned tuple is (winner, loser)
+    pub fn play(&mut self, mut dice: impl Roll) -> Option<(&Player, &Player)> {
+        for _ in 1.. {
+            let values = dice.roll(3);
+            self.roll_count += 3;
+
+            if self.player1.play(&values) >= 1000 {
+                return Some((&self.player1, &self.player2));
+            }
+
+            let values = dice.roll(3);
+            self.roll_count += 3;
+
+            if self.player2.play(&values) >= 1000 {
+                return Some((&self.player2, &self.player1));
+            }
+        }
+
+        None
     }
 }
 
 fn main() {
-    let mut game = Game::new(1, 1);
+    // Given input, player1 starts at 7, player2 starts at 3.
+    let mut game = Game::new(7, 3);
     let dice = DeterministicDice::default();
-    game.play(dice);
+    let (_winner, loser) = game.play(dice).unwrap();
+    dbg!(loser.score * game.roll_count);
 }
 
 #[cfg(test)]
@@ -69,8 +97,9 @@ mod tests {
 
     #[test]
     fn test_deterministic_game() {
-        let mut game = Game::new(1, 1);
+        let mut game = Game::new(4, 8);
         let dice = DeterministicDice::default();
-        let winner = game.play(dice);
+        let (_winner, loser) = game.play(dice).unwrap();
+        assert_eq!(739785, loser.score * game.roll_count);
     }
 }
